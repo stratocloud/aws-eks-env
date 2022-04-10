94fb3d12e6bb2975559e27a55b6155b6b4a0ee15
Ajay Chava <cloudfreak@Ajays-MBP.lan>
2022-04-10 11:54:06 -0500

    Updating with Terraform fmt

--- version.tf
+++ version.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source   = "hashicorp/aws"
-      version  = "~> 3.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
   required_version = ">= 1.1.8"
