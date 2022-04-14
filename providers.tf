a16c8640fa6cc6300ef66dc0607870f5652d37aa
stratocloud <stratocloud01@gmail.com>
2022-04-14 09:50:18 -0500

    removing  manage_aws_auth_configmap = true

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.eks_cluster_auth.default.token
+
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws"
