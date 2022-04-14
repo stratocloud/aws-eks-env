f48ee2bcdddb15e3ef405394888f0fb5c2576493
stratocloud <stratocloud01@gmail.com>
2022-04-14 09:41:44 -0500

    Fix: Error: The configmap aws-auth does not exist when deploying an EKS cluster with manage_aws_auth_configmap = true

--- datasources.tf
+++ datasources.tf
@@ -5,3 +5,11 @@ data "aws_caller_identity" "current" {
 data "aws_region" "current" {
 
 }
+
+data "aws_eks_cluster" "default" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = local.cluster_name
+}
