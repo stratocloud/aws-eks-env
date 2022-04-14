68a96efbe7aff09109eacdc7fda647f2a605cf54
stratocloud <stratocloud01@gmail.com>
2022-04-14 09:46:20 -0500

    Updating Kubernetes provider config

--- providers.tf
+++ providers.tf
@@ -7,9 +7,7 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.default.token
+  token                  = data.eks_cluster_auth.default.token
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws"
