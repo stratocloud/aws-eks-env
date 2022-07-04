5510d240397f5fa69d84c9dbf01db74e63d62758
stratocloud <102773960+stratocloud@users.noreply.github.com>
2022-07-04 03:14:47 -0500

    Update datasource_eks.tf

--- datasource_eks.tf
+++ datasource_eks.tf
@@ -27,3 +27,29 @@ data "aws_ami" "eks_default_bottlerocket" {
     values = ["bottlerocket-aws-k8s-${local.cluster_version}-x86_64-*"]
   }
 }
+
+// Create an IAM role for cluster admins and only allow a user created by Vault to assume it.
+data "aws_iam_policy_document" "assume_policy" {
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    condition {
+      test     = "ForAnyValue:StringLike"
+      variable = "aws:PrincipalArn"
+      values = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${var.admin_user}"
+      ]
+    }
+  }
+}
